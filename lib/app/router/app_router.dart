8240fec5c626ee348e270e75607e451649013a21
Dagim Abraham <dagimabraham79@gmail.com>
2025-12-25 23:18:05 +0300

    chore: rebrand to BlueFeed (UI-only)

---
+++ lib/app/router/app_router.dart
@@ -0,0 +1,33 @@
+import 'package:bluefeed/core/auth_gate.dart';
+import 'package:bluefeed/main.dart';
+import 'package:bluefeed/screens/login.dart';
+import 'package:bluefeed/screens/profile.dart';
+import 'package:bluefeed/screens/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/auth_gate',
+  routes: [
+    GoRoute(
+      path: '/auth_gate',
+      builder: (context, state) => const AuthGate(),
+    ),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const BlueFeedApp(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const Login(),
+    ),
+    GoRoute(
+      path: '/signup',
+      builder: (context, state) => const Signup(),
+    ),
+    GoRoute(
+      path: '/profile',
+      builder: (context, state) => const Profile(),
+    )
+  ],
+);
